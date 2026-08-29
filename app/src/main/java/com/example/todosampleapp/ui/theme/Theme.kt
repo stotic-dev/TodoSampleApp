1f24b136057f0574af42c47d3c48c97b7ae1995e
stotic-dev <taichis844@gmail.com>
2026-08-29 16:11:51 +0900

    ciのlint設定修正

--- app/src/main/java/com/example/todosampleapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/todosampleapp/ui/theme/Theme.kt
@@ -266,9 +266,7 @@ fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content:
-        @Composable()
-        () -> Unit,
+    content: @Composable () -> Unit,
 ) {
     val colorScheme =
         when {
